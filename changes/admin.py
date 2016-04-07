b5d5ac03640854d316d4f9588affb886d61e0d47
Mike Jones <mike@westerncapelabs.com>
2016-04-07 14:06:39 +0200

    Updated admin listings

--- changes/admin.py
+++ changes/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
+
+class ChangeAdmin(admin.ModelAdmin):
+    list_display = [
+        "id", "action", "mother_id", "validated", "source",
+        "created_at", "updated_at", "created_by", "updated_by"]
+    list_filter = ["source", "validated", "created_at"]
+    search_fields = ["mother_id", "to_addr"]
+
 from .models import Change
 
-admin.site.register(Change)
+admin.site.register(Change, ChangeAdmin)
--- registrations/admin.py
+++ registrations/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
+
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = [
+        "id", "stage", "validated", "mother_id", "source",
+        "created_at", "updated_at", "created_by", "updated_by"]
+    list_filter = ["source", "validated", "created_at"]
+    search_fields = ["mother_id", "to_addr"]
+
+
 from .models import Source, Registration
 
 admin.site.register(Source)
-admin.site.register(Registration)
+admin.site.register(Registration, RegistrationAdmin)
