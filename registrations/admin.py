540c0130ccca7d7d7cf51ddbe251652a5c46364e
Mike Jones <mike@westerncapelabs.com>
2016-07-01 08:50:16 +0200

    Add SubscriptionRequestAdmin - reviewed over shoulder by @gsvr

--- registrations/admin.py
+++ registrations/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Source, Registration
+from .models import Source, Registration, SubscriptionRequest
 
 
 class RegistrationAdmin(admin.ModelAdmin):
@@ -9,5 +9,15 @@ class RegistrationAdmin(admin.ModelAdmin):
     list_filter = ["source", "validated", "created_at"]
     search_fields = ["mother_id", "to_addr"]
 
+
+class SubscriptionRequestAdmin(admin.ModelAdmin):
+    list_display = [
+        "id", "contact", "messageset", "next_sequence_number", "lang",
+        "schedule", "created_at", "updated_at"]
+    list_filter = ["messageset", "created_at"]
+    search_fields = ["contact"]
+
+
 admin.site.register(Source)
 admin.site.register(Registration, RegistrationAdmin)
+admin.site.register(SubscriptionRequest, SubscriptionRequestAdmin)
