76359ee175c078395389703227492a33541c7fca
Mike Jones <mike@westerncapelabs.com>
2016-08-23 22:11:19 +0200

    Add changes list and filter endpoint

--- changes/urls.py
+++ changes/urls.py
@@ -3,6 +3,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
+router.register(r'changes', views.ChangeGetViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browseable API.
