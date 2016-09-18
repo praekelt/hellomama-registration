8d70006dd48d56e6bc2a9c6a61b930f4cfd5819e
Simon de Haan <simon@praekeltfoundation.org>
2016-09-18 21:12:15 +0200

    fix flake8

--- registrations/management/commands/fire_subscription_hook.py
+++ registrations/management/commands/fire_subscription_hook.py
@@ -1,5 +1,4 @@
 from django.core.management.base import BaseCommand, CommandError
-from django.conf import settings
 from django.contrib.auth.models import User
 
 from rest_hooks.signals import raw_hook_event
