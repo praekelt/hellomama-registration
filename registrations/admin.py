8e68c5e6350ae20f78bf6b296863e34c606e2964
Rudi Giesler <rudi@praekelt.com>
2016-10-26 15:25:34 +0200

    Add form to repopulate metrics view to repopulate the metrics

--- registrations/admin.py
+++ registrations/admin.py
@@ -1,7 +1,24 @@
-from django.contrib import admin
+from django import forms
+from django.contrib import admin, messages
+from django.contrib.admin import helpers
 from django.conf.urls import url
+from django.shortcuts import redirect
 from django.template.response import TemplateResponse
+
+from hellomama_registration.utils import get_available_metrics
 from .models import Source, Registration, SubscriptionRequest
+
+
+class RepopulateMetricsForm(forms.Form):
+    amqp_url = forms.CharField(
+        label='AMQP URL', initial='amqp://guest:guest@localhost:5672/%2F',
+        widget=forms.TextInput(attrs={'size': 80}))
+    metric_names = forms.MultipleChoiceField(
+        choices=[(m, m) for m in get_available_metrics()],
+        initial=get_available_metrics())
+    graphite_retentions = forms.CharField(
+        label='Graphite Retentions', initial='1m:1d,5m:1y,1h:5y',
+        widget=forms.TextInput(attrs={'size': 80}))
 
 
 class RegistrationAdmin(admin.ModelAdmin):
@@ -20,11 +37,23 @@ class RegistrationAdmin(admin.ModelAdmin):
         ] + urls
 
     def repopulate_metrics(self, request):
+
+        if request.method == 'POST':
+            form = RepopulateMetricsForm(request.POST)
+            if form.is_valid:
+                # TODO: Start celery task to repopulate metrics
+                messages.success(request, 'Metrics repopulation started')
+                return redirect('admin:registrations_registration_changelist')
+        else:
+            form = RepopulateMetricsForm()
         context = dict(
             self.admin_site.each_context(request),
             title='Repopulate Metrics',
             opts=self.model._meta,
-            form=None
+            form=form,
+            adminform=helpers.AdminForm(
+                form, [(None, {'fields': form.base_fields})],
+                self.get_prepopulated_fields(request)),
         )
         return TemplateResponse(
             request,
