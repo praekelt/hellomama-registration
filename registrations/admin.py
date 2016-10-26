43688c56a84a50fb27f6a41f0468b0a648889243
Rudi Giesler <rudi@praekelt.com>
2016-10-26 14:41:08 +0200

    Add button to django admin for repopulating metrics

--- registrations/admin.py
+++ registrations/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf.urls import url
+from django.template.response import TemplateResponse
 from .models import Source, Registration, SubscriptionRequest
 
 
@@ -8,6 +10,26 @@ class RegistrationAdmin(admin.ModelAdmin):
         "created_at", "updated_at", "created_by", "updated_by"]
     list_filter = ["source", "validated", "created_at"]
     search_fields = ["mother_id", "to_addr"]
+
+    def get_urls(self):
+        urls = super(RegistrationAdmin, self).get_urls()
+        return [
+            url(r'^repopulate_registration_metrics/$',
+                self.admin_site.admin_view(self.repopulate_metrics),
+                name='repopulate_registration_metrics'),
+        ] + urls
+
+    def repopulate_metrics(self, request):
+        context = dict(
+            self.admin_site.each_context(request),
+            title='Repopulate Metrics',
+            opts=self.model._meta,
+            form=None
+        )
+        return TemplateResponse(
+            request,
+            "admin/registrations/registration/repopulate_metrics.html",
+            context)
 
 
 class SubscriptionRequestAdmin(admin.ModelAdmin):
