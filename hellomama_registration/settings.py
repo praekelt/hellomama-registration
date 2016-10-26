d0fb0cd3f776c54f123f4d7bb9d9fe5083240782
Rudi Giesler <rudi@praekelt.com>
2016-10-26 13:59:43 +0200

    Change to new TEMPLATES settings var

--- hellomama_registration/settings.py
+++ hellomama_registration/settings.py
@@ -119,6 +119,27 @@ STATIC_URL = '/static/'
 #     "django.core.context_processors.request",
 # )
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'debug': DEBUG,
+        },
+    },
+]
+
 # Sentry configuration
 RAVEN_CONFIG = {
     # DevOps will supply you with this.
