a30f087f89c2e0640e50a129f150fdc143e4def3
Kaitlyn Crawford <kaitlyn@praekelt.com>
2016-11-22 13:14:28 +0200

    Move action to regenerate personnel code from Registration to Record

--- uniqueids/admin.py
+++ uniqueids/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import Record
 
 
@@ -9,6 +8,32 @@ class RecordAdmin(admin.ModelAdmin):
         "id", "identity", "write_to", "created_at", "updated_at"]
     list_filter = ["write_to", "created_at"]
     search_fields = ["identity", "write_to"]
+    actions = ["regenerate_personnel_code"]
 
+    def regenerate_personnel_code(self, request, queryset):
+        created = 0
+        skipped = 0
+        for record in queryset:
+            if record.write_to != "personnel_code":
+                skipped += 1
+                continue
+            Record.objects.create(
+                identity=record.identity, write_to="personnel_code",
+                length=record.length, created_by=request.user)
+            created += 1
+        if created == 1:
+            created_text = "%s Record was" % created
+        else:
+            created_text = "%s Records were" % created
+        if skipped == 1:
+            skipped_text = "%s Record was" % skipped
+        else:
+            skipped_text = "%s Records were" % skipped
+        self.message_user(
+            request, "%s successfully changed. %s skipped because they are "
+            "not a HCW." % (created_text, skipped_text))
+
+    regenerate_personnel_code.short_description = "Change code and "\
+        "SMS (personnel code only)"
 
 admin.site.register(Record, RecordAdmin)
