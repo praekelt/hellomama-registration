5a17b9342b83182bbb5ce8c5bf4efa74329b3211
Kaitlyn Crawford <kaitlyn@praekelt.com>
2016-11-22 12:49:36 +0200

    Add quality of life improvements to Record Admin (search, filtering, etc)

--- uniqueids/admin.py
+++ uniqueids/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Record
 
-admin.site.register(Record)
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = [
+        "id", "identity", "write_to", "created_at", "updated_at"]
+    list_filter = ["write_to", "created_at"]
+    search_fields = ["identity", "write_to"]
+
+
+admin.site.register(Record, RecordAdmin)
