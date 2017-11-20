9d9eee7d21b2710a00152286ea689e36bf2f8dca
Kaitlyn Crawford <kaitlyn@praekelt.com>
2017-11-20 15:19:23 +0200

    Build spreadsheet seperately to retrieving identity info

--- reports/tasks/msisdn_message_report.py
+++ reports/tasks/msisdn_message_report.py
@@ -18,7 +18,15 @@ class GenerateMSISDNMessageReport(BaseTask):
         task_status.status = ReportTaskStatus.RUNNING
         task_status.save()
 
-        spreadsheet = self.create_spreadsheet(msisdns)
+        self.is_client = IdentityStoreApiClient(
+            settings.IDENTITY_STORE_TOKEN,
+            settings.IDENTITY_STORE_URL,
+        )
+
+        data = self.retrieve_identity_info(msisdns)
+
+        spreadsheet = self.populate_spreadsheet(data)
+
         output_file = generate_random_filename()
         spreadsheet.save(output_file)
 
@@ -28,15 +36,35 @@ class GenerateMSISDNMessageReport(BaseTask):
 
         # TODO: Use SendEmail task to send the email
 
-    def create_spreadsheet(self, msisdns):
+    def retrieve_identity_info(self, msisdns):
         logger = self.get_logger()
+
+        data = {}
+        for msisdn in msisdns:
+            response = self.is_client.get_identity_by_address('msisdn', msisdn)
+            results = list(response['results'])
+
+            if len(results) < 1:
+                logger.info(
+                    'No results from identity store for {0}'.format(msisdn))
+                data[msisdn] = {}
+                continue
+
+            if len(results) > 1:
+                logger.info(
+                    'Multiple results from identity store for {0}'
+                    .format(msisdn))
+                data[msisdn] = {}
+                continue
+
+            data[msisdn] = {'id': results[0]['id'],
+                            'created_at': results[0]['created_at']}
+
+        return data
+
+    def populate_spreadsheet(self, data):
         workbook = ExportWorkbook()
         sheet = workbook.add_sheet('Data for study cohort', 0)
-
-        identity_store = IdentityStoreApiClient(
-            settings.IDENTITY_STORE_TOKEN,
-            settings.IDENTITY_STORE_URL,
-        )
 
         sheet.set_header([
             'Phone number',
@@ -46,29 +74,13 @@ class GenerateMSISDNMessageReport(BaseTask):
             'Message type',
             'Message 1: content',
             'Message 1: date sent',
-            'Message 1: status',
+            'Message 1: status'
         ])
 
-        for msisdn in msisdns:
-            response = identity_store.get_identity_by_address('msisdn', msisdn)
-            results = list(response['results'])
-
-            if len(results) < 1:
-                logger.info(
-                    'No results from identity store for {0}'.format(msisdn))
-                continue
-
-            if len(results) > 1:
-                logger.info(
-                    'Multiple results from identity store for {0}'
-                    .format(msisdn))
-                continue
-
-            identity = results[0]
-
+        for msisdn in data:
             sheet.add_row({
                 'Phone number': msisdn,
-                'Date registered': identity['created_at'],
+                'Date registered': data[msisdn]['created_at']
             })
 
         return workbook
