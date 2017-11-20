f66c0683d224c52d8f8b54d979fad4b46589bb41
Kaitlyn Crawford <kaitlyn@praekelt.com>
2017-11-20 18:06:46 +0200

    Update tests for creating spreadsheet

--- reports/tests/test_msisdn_message_report.py
+++ reports/tests/test_msisdn_message_report.py
@@ -1,4 +1,5 @@
 import responses
+from datetime import datetime
 
 from django.test import TestCase, override_settings
 from reports.tasks.msisdn_message_report import generate_msisdn_message_report
@@ -20,41 +21,98 @@ class GenerateMSISDNMessageReportTest(TestCase):
             match_querystring=True,
         )
 
-    def test_create_spreadsheet_returns_spreadsheet(self):
-        spreadsheet = generate_msisdn_message_report.create_spreadsheet([])
+    def test_populate_spreadsheet_returns_spreadsheet(self):
+        spreadsheet = generate_msisdn_message_report.populate_spreadsheet(
+            {}, 0)
         self.assertTrue(isinstance(spreadsheet, ExportWorkbook))
 
-    @responses.activate
-    def test_create_spreadsheet_includes_details(self):
-        self.add_response_identity_store_search([{'created_at': '2017-01-01'}])
+    def test_populate_spreadsheet_has_headers_for_messages(self):
+        spreadsheet = generate_msisdn_message_report.populate_spreadsheet(
+            {}, 2)
 
-        spreadsheet = generate_msisdn_message_report.create_spreadsheet(
-            ['+2340000000'],
-        )
+        self.assertEqual(
+            spreadsheet._workbook.active['F1'].value, 'Message 1: content')
+        self.assertEqual(
+            spreadsheet._workbook.active['G1'].value, 'Message 1: date sent')
+        self.assertEqual(
+            spreadsheet._workbook.active['H1'].value, 'Message 1: status')
+        self.assertEqual(
+            spreadsheet._workbook.active['I1'].value, 'Message 2: content')
+        self.assertEqual(
+            spreadsheet._workbook.active['J1'].value, 'Message 2: date sent')
+        self.assertEqual(
+            spreadsheet._workbook.active['K1'].value, 'Message 2: status')
+
+    def test_populate_spreadsheet_skips_if_identity_is_missing(self):
+        spreadsheet = generate_msisdn_message_report.populate_spreadsheet(
+            {"+2340000000": {
+                "reg_date": datetime(2017, 01, 01, 00, 00, 00),
+                "facility": "Somewhere",
+                "preg_week": 16,
+                "msg_type": "text"
+            }}, 0)
 
         self.assertEqual(
             spreadsheet._workbook.active['A2'].value, '+2340000000')
         self.assertEqual(
-            spreadsheet._workbook.active['B2'].value, '2017-01-01')
+            spreadsheet._workbook.active['B2'].value, None)
+        self.assertEqual(
+            spreadsheet._workbook.active['C2'].value, None)
 
-    @responses.activate
-    def test_create_spreadsheet_discards_msisdn_no_results(self):
-        self.add_response_identity_store_search([])
+    def test_populate_spreadsheet_includes_registration_details(self):
+        spreadsheet = generate_msisdn_message_report.populate_spreadsheet(
+            {"+2340000000": {
+                "id": "54cc71b7-533f-4a83-93c1-e02340000000",
+                "reg_date": datetime(2017, 01, 01, 00, 00, 00),
+                "facility": "Somewhere",
+                "preg_week": 16,
+                "msg_type": "text"
+            }}, 0)
 
-        spreadsheet = generate_msisdn_message_report.create_spreadsheet(
-            ['+2340000000'],
-        )
+        self.assertEqual(
+            spreadsheet._workbook.active['A2'].value, '+2340000000')
+        self.assertEqual(
+            spreadsheet._workbook.active['B2'].value,
+            datetime(2017, 01, 01, 00, 00, 00))
+        self.assertEqual(
+            spreadsheet._workbook.active['C2'].value, 'Somewhere')
+        self.assertEqual(
+            spreadsheet._workbook.active['D2'].value, 16)
+        self.assertEqual(
+            spreadsheet._workbook.active['E2'].value, 'text')
+        self.assertEqual(
+            spreadsheet._workbook.active['F2'].value, None)
 
-        self.assertEqual(spreadsheet._workbook.active['A2'].value, None)
+    def test_populate_spreadsheet_includes_messages(self):
+        spreadsheet = generate_msisdn_message_report.populate_spreadsheet(
+            {"+2340000000": {
+                "id": "54cc71b7-533f-4a83-93c1-e02340000000",
+                "messages": [{
+                    "content": "Test message",
+                    "date_sent": datetime(2017, 01, 01, 00, 00, 00),
+                    "status": "Delivered"
+                }, {
+                    "content": "Another test message",
+                    "date_sent": datetime(2017, 01, 02, 00, 00, 00),
+                    "status": "Undelivered"
+                }]
+            }}, 2)
 
-    @responses.activate
-    def test_create_spreadsheet_discards_msisdn_multiple_results(self):
-        self.add_response_identity_store_search(
-            [{'details': '1'}, {'details': '2'}]
-        )
-
-        spreadsheet = generate_msisdn_message_report.create_spreadsheet(
-            ['+2340000000'],
-        )
-
-        self.assertEqual(spreadsheet._workbook.active['A2'].value, None)
+        self.assertEqual(
+            spreadsheet._workbook.active['A2'].value, '+2340000000')
+        self.assertEqual(
+            spreadsheet._workbook.active['D2'].value, '')
+        self.assertEqual(
+            spreadsheet._workbook.active['F2'].value, 'Test message')
+        self.assertEqual(
+            spreadsheet._workbook.active['G2'].value,
+            datetime(2017, 01, 01, 00, 00, 00))
+        self.assertEqual(
+            spreadsheet._workbook.active['H2'].value, 'Delivered')
+        self.assertEqual(
+            spreadsheet._workbook.active['I2'].value, 'Another test message')
+        self.assertEqual(
+            spreadsheet._workbook.active['J2'].value,
+            datetime(2017, 01, 02, 00, 00, 00))
+        self.assertEqual(
+            spreadsheet._workbook.active['K2'].value, 'Undelivered')
