d9bcd837e05b1dbee392adea601775db84179128
Kaitlyn Crawford <kaitlyn@praekelt.com>
2017-11-22 11:06:57 +0200

    Add tests for retrieveing identity data

--- reports/tests/test_msisdn_message_report.py
+++ reports/tests/test_msisdn_message_report.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from django.test import TestCase, override_settings
 from reports.tasks.msisdn_message_report import generate_msisdn_message_report
 from reports.utils import ExportWorkbook
+from seed_services_client import IdentityStoreApiClient
 
 
 @override_settings(
@@ -11,16 +12,18 @@ from reports.utils import ExportWorkbook
     IDENTITY_STORE_TOKEN='idstoretoken',
 )
 class GenerateMSISDNMessageReportTest(TestCase):
-    def add_response_identity_store_search(self, results):
+    def add_response_identity_store_search(self, msisdn, results):
         responses.add(
             responses.GET,
             ('http://identity-store/identities/search/'
-                '?details__addresses__msisdn=%2B2340000000'),
+                '?details__addresses__msisdn=%s' % msisdn),
             json={'results': results},
             content_type='application/json',
             match_querystring=True,
         )
 
+
+class PopulateSpreadsheetTest(GenerateMSISDNMessageReportTest):
     def test_populate_spreadsheet_returns_spreadsheet(self):
         spreadsheet = generate_msisdn_message_report.populate_spreadsheet(
             {}, 0)
@@ -116,3 +119,66 @@ class GenerateMSISDNMessageReportTest(TestCase):
             datetime(2017, 01, 02, 00, 00, 00))
         self.assertEqual(
             spreadsheet._workbook.active['K2'].value, 'Undelivered')
+
+
+class RetrieveIdentityInfoTest(GenerateMSISDNMessageReportTest):
+    def setUp(self):
+        self.is_client = IdentityStoreApiClient('idstoretoken',
+                                                'http://identity-store/')
+
+    @responses.activate
+    def test_retrieve_identity_adds_id(self):
+        self.add_response_identity_store_search('%2B2340000000', [
+            {'id': '54cc71b7-533f-4a83-93c1-e02340000000',
+                'created_at': '2017-01-01'}])
+
+        data = generate_msisdn_message_report.retrieve_identity_info(
+            self.is_client, ['+2340000000'])
+
+        self.assertEqual(data.keys(), ['+2340000000'])
+        self.assertItemsEqual(data['+2340000000'].keys(), ['id', 'created_at'])
+        self.assertEqual(data['+2340000000']['id'],
+                         '54cc71b7-533f-4a83-93c1-e02340000000')
+        self.assertEqual(data['+2340000000']['created_at'], '2017-01-01')
+
+    @responses.activate
+    def test_retrieve_identity_works_with_multiple_msisdns(self):
+        self.add_response_identity_store_search('%2B2340000000', [
+            {'id': '54cc71b7-533f-4a83-93c1-e02340000000',
+                'created_at': '2017-01-01'}])
+        self.add_response_identity_store_search('%2B2341111111', [
+            {'id': '54cc71b7-533f-4a83-93c1-e02341111111',
+                'created_at': '2017-01-02'}])
+
+        data = generate_msisdn_message_report.retrieve_identity_info(
+            self.is_client, ['+2340000000'])
+
+        self.assertEqual(data.keys(), ['+2340000000'])
+        self.assertItemsEqual(data['+2340000000'].keys(), ['id', 'created_at'])
+        self.assertEqual(data['+2340000000']['id'],
+                         '54cc71b7-533f-4a83-93c1-e02340000000')
+        self.assertEqual(data['+2340000000']['created_at'], '2017-01-01')
+
+    @responses.activate
+    def test_identity_data_empty_if_none_found(self):
+        self.add_response_identity_store_search('%2B2340000000', [])
+
+        data = generate_msisdn_message_report.retrieve_identity_info(
+            self.is_client, ['+2340000000'])
+
+        self.assertEqual(data.keys(), ['+2340000000'])
+        self.assertEqual(data['+2340000000'], {})
+
+    @responses.activate
+    def test_identity_data_empy_if_multiple_found(self):
+        self.add_response_identity_store_search('%2B2340000000', [
+            {'id': '54cc71b7-533f-4a83-93c1-e02340000000',
+                'created_at': '2017-01-01'},
+            {'id': '54cc71b7-533f-4a83-93c1-e02341111111',
+                'created_at': '2017-01-02'}])
+
+        data = generate_msisdn_message_report.retrieve_identity_info(
+            self.is_client, ['+2340000000'])
+
+        self.assertEqual(data.keys(), ['+2340000000'])
+        self.assertEqual(data['+2340000000'], {})
