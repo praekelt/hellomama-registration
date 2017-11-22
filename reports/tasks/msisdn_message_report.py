7fdbb79684e4a3fcb915405bc8543e6327895475
Kaitlyn Crawford <kaitlyn@praekelt.com>
2017-11-22 09:25:22 +0200

    Pass services clients in as args

--- reports/tasks/msisdn_message_report.py
+++ reports/tasks/msisdn_message_report.py
@@ -23,21 +23,21 @@ class GenerateMSISDNMessageReport(BaseTask):
         task_status.status = ReportTaskStatus.RUNNING
         task_status.save()
 
-        self.is_client = IdentityStoreApiClient(
+        is_client = IdentityStoreApiClient(
             settings.IDENTITY_STORE_TOKEN,
             settings.IDENTITY_STORE_URL,
         )
-        self.ms_client = MessageSenderApiClient(
+        ms_client = MessageSenderApiClient(
             settings.MESSAGE_SENDER_TOKEN,
             settings.MESSAGE_SENDER_URL,
         )
 
-        data = self.retrieve_identity_info(msisdns)
+        data = self.retrieve_identity_info(is_client, msisdns)
 
-        data = self.retrieve_registration_info(data)
+        data = self.retrieve_registration_info(is_client, data)
 
-        (data, list_length) = self.retrieve_messages(data, start_date,
-                                                     end_date)
+        (data, list_length) = self.retrieve_messages(ms_client, data,
+                                                     start_date, end_date)
 
         spreadsheet = self.populate_spreadsheet(data, list_length)
 
@@ -60,12 +60,12 @@ class GenerateMSISDNMessageReport(BaseTask):
                 'recipients': email_recipients,
                 'task_status_id': task_status_id})
 
-    def retrieve_identity_info(self, msisdns):
+    def retrieve_identity_info(self, is_client, msisdns):
         logger = self.get_logger()
 
         data = {}
         for msisdn in msisdns:
-            response = self.is_client.get_identity_by_address('msisdn', msisdn)
+            response = is_client.get_identity_by_address('msisdn', msisdn)
             results = list(response['results'])
 
             if len(results) < 1:
@@ -86,7 +86,7 @@ class GenerateMSISDNMessageReport(BaseTask):
 
         return data
 
-    def retrieve_registration_info(self, data):
+    def retrieve_registration_info(self, is_client, data):
         logger = self.get_logger()
 
         for msisdn in data:
@@ -112,7 +112,7 @@ class GenerateMSISDNMessageReport(BaseTask):
             # Get facility info from the operator's identity
             operator_id = registration.data.get('operator_id', None)
             if operator_id is not None:
-                operator_identity = self.is_client.get_identity(operator_id)
+                operator_identity = is_client.get_identity(operator_id)
                 data[msisdn]['facility'] = operator_identity.get(
                         'details', {}).get('facility_name', "")
             else:
@@ -122,14 +122,14 @@ class GenerateMSISDNMessageReport(BaseTask):
 
         return data
 
-    def retrieve_messages(self, data, start_date, end_date):
+    def retrieve_messages(self, ms_client, data, start_date, end_date):
         logger = self.get_logger()
 
         longest_list = 0
         for msisdn in data:
             message_list = []
 
-            response = self.ms_client.get_outbounds({
+            response = ms_client.get_outbounds({
                 "to_identity": data[msisdn]['id'],
                 "after": start_date.strftime("%Y-%m-%dT00:00:00"),
                 "before": end_date.strftime("%Y-%m-%dT00:00:00")
