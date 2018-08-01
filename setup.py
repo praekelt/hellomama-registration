49e8d72386b0928a0a87eeab2ba0c8ba3a538734
Erik Harding <erik@praekelt.org>
2018-08-01 08:40:01 +0200

    version bump 0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hellomama-registration",
-    version="0.1.6",
+    version="0.1.7",
     url='http://github.com/praekelt/hellomama-registration',
     license='BSD',
     author='Praekelt Foundation',
